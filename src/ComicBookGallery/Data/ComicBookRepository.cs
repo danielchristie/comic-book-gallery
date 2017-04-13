71e2af35e3ba1fcba36acf7535ccb0d932d33ded
Daniel Christie <vaylain@gmail.com>
2017-04-13 10:11:53 -0700

    Added Comic Books List View

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -49,6 +49,10 @@ namespace ComicBookGallery.Data {
                 Favorite = false
             }
         };
+        public ComicBook[] GetComicBooks() {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id) {
             ComicBook comicBookToReturn = null;
             foreach (var comicBook in _comicBooks) {
